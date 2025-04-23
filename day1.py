bd7163501090a6c234fae29df6c5a4426b9abf6f
AndersonRaulino <anderson.raul4631@gmail.com>
2025-04-23 14:30:56 -0300

    Day1

---
+++ day1.py
@@ -0,0 +1,5 @@
+from time import sleep #Função para dar um intervalo entre os códigos.
+
+saudacao = print('Hello World!') #Exibe a mensagem 'Hello World' no console, para quebrar a maldição.
+sleep(0.8)#Intervalo de 0.8s.
+print("Ebaaaaa!!! Quebrei a maldição!!!")

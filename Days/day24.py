f6fb051cdc07c5139e6cc2d85554a59747c6bc14
AndersonRaulino <anderson.raul4631@gmail.com>
2025-05-16 17:10:35 -0300

    Integrando um split

--- Days/day24.py
+++ Days/day24.py
@@ -5,3 +5,11 @@ lista = ["Olá", "Mundo.", "Quebrei", "a", "maldição."]
 
 print(f"Lista sem conversão: {lista}")
 print(f"Lista com conversão: {converterPalavras(lista)}")
+
+
+# Separando novamento as palavras, utilizando um split() dentro de uma nova função
+def inverterConversão(nova):
+    x = nova.split(" ")
+    return x
+
+print(f"Separação da frase: {inverterConversão(converterPalavras(lista))}")

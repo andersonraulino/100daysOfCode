e438e44911a366e549ca62f1faaa8bb17d833a13
AndersonRaulino <anderson.raul4631@gmail.com>
2025-05-16 16:50:47 -0300

    Day 24

---
+++ Days/day24.py
@@ -0,0 +1,8 @@
+def converterPalavras(palavra):
+    for i in lista:
+        return " ".join(palavra)
+
+lista = ["Olá", "Mundo.", "Quebrei", "a", "maldição."]
+
+print(f"Lista sem conversão: {lista}")
+print(f"Lista com conversão: {converterPalavras(lista)}")
